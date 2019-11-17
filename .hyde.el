5b54886e5bace20d030eae9ec4d3aa846cc75d81
Annard Brouwer <me@annard.me>
2019-11-17 22:33:27 +0000

    Admin cleanup

--- .hyde.el
+++ .hyde.el
@@ -1,6 +1,4 @@
-(setq hyde/git/remote "origin" ; The name of the remote to which we should push
-      hyde/git/remote "master" ; The name of the branch on which your blog resides
-      hyde/jekyll-command "rvm use 2.6.3@jekyll && bundle exec jekyll build"
+(setq hyde/jekyll-command "rvm use 2.6.3@jekyll && bundle exec jekyll build"
       hyde/jekyll-serve "rvm use 2.6.3@jekyll && bundle exec jekyll serve"
       hyde-custom-params '(("category" "boat"))
       )
